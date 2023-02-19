ee9be15a4629c978ec54bf405e0cc2be55d34143
raf <rnm1816@gmail.com>
2023-02-19 05:24:35 +0200

    projects

---
+++ react-calci/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Main from './Main'
+import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.css'
+function App() {
+  return (
+    <div className="App">
+         <Main/>
+    </div>
+  );
+}
+
+export default App;
+
+
+
