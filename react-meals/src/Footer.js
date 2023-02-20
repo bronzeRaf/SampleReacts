143b12fd272cd7a6e58942872e3f1a66f5457fce
raf <rnm1816@gmail.com>
2023-02-20 04:11:19 +0200

    meals

---
+++ react-meals/src/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Footer(){
+    return <div style={{backgroundColor:'brown'}}>
+        <p style={{color:'white'}}>Designed and Developed By</p>
+        <p style={{color:'white'}}>Raf Brouzos</p>
+    </div>
+}
+export default Footer
